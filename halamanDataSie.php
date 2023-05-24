b7e1679e2b3d78f77d473b6dc21174595b887f5e
BrianMohamadSafiudin <brianms2004@gmail.com>
2023-05-25 05:25:15 +0700

    frontend

--- halamanDataSie.php
+++
@@ -1,4 +0,0 @@
-<?php
-$heading= 'Data SIE Program Kerja';
-$judulProker = 'Safari Dakwah';
-require "views/dataSie.view.php";
--- tambahTugas.php
+++
@@ -1,4 +0,0 @@
-<?php
-$heading= 'Halaman Program Kerja';
-$judulProker = 'Safari Dakwah';
-require "views/tambahTugas.view.php";
