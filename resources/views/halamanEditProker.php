c5855a5f14e1fe0a1fc52be29c85e1703e458777
= FadlilaAfida <= fadlilaafida01@gmail.com>
2023-05-25 13:05:24 +0700

    frontend-admin 3 fadlila

--- resources/views/halamanEditProker.php
+++
@@ -1,4 +0,0 @@
-<?php
-$heading= 'Halaman Program Kerja';
-$judulProker = 'Safari Dakwah';
-require "views/halamanEditProker.view.php";
---
+++ resources/views/tambahDataSie.blade.php
@@ -0,0 +1,36 @@
+@extends('views.partials.head')
+
+@php
+    $dashboardPlaceholder = "Halaman Program Kerja";
+    $judulProker = 'Safari Dakwah';
+@endphp
+
+@section('content')
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+    @auth()
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+    @include('views.partials.sidebar')
+    @endauth
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+            @include('views.partials.topbar')
+            @include('views.tambahDataSie_view')
+            {{-- @include('layouts.footers.guest') --}}
+
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+@endsection
