6549c596ba2ce756cc41c7c06ee9719d237ea15d
= FebryanRizki24 <= febryanrizki24@gmail.com>
2023-05-30 18:10:12 +0700

    backend

--- resources/views/views/tambahTugas_view.blade.php
+++ resources/views/views/tambahTugas_view.blade.php
@@ -27,26 +27,58 @@
                         </ul>
                         <hr class="content-divider">
 
-                        <h5 class=" mb-2 text-gray-900 font-weight-bolder">Edit Data User</h5>
+                        <h5 class=" mb-2 text-gray-900 font-weight-bolder">Tambah Tugas Program Kerja</h5>
                         
                         <div class="card-body">
-                            <form>
-                                <div class="form-group mb-4">
+                            <form method="post" action="{{ route('tugasproker') }}" enctype="multipart/form-data" autocomplete="off">
+                                @csrf
+
+                                                @if (session('status'))
+                                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                    {{ session('status') }}
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                @endif
+                                <div class="mb-4 form-group{{ $errors->has('namatugas') ? ' has-danger' : '' }}">
                                     <label class="text-gray-900 font-weight-bold " for="exampleInputEmail1 ">Nama Tugas</label>
-                                    <input type="text" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan nama tugas">
+                                    <input type="text" name="namatugas" class="form-control form-control-alternative{{ $errors->has('namatugas') ? ' is-invalid' : '' }}" placeholder="Masukkan Nama Tugas" value="" id="input-namatugas" required/>
+                                    @if ($errors->has('namatugas'))
+                                        <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('namatugas') }}</strong>
+                                        </span>
+                                    @endif
                                 </div>
-                                <div class="form-group mb-4">
+                                <div class="mb-4 form-group{{ $errors->has('sie') ? ' has-danger' : '' }}">
                                     <label class="text-gray-900 font-weight-bold" for="exampleInputPassword1">SIE</label>
-                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Masukkan nama Sie">
+                                    <input type="text" name="sie" class="form-control form-control-alternative{{ $errors->has('sie') ? ' is-invalid' : '' }}" placeholder="Masukkan Nama Sie" value="" id="input-sie" required/>
+                                    @if ($errors->has('sie'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('sie') }}</strong>
+                                        </span>
+                                    @endif
                                 </div>
-                                <div class="form-group mb-4">
+                                <div class="mb-4 form-group{{ $errors->has('tenggatwaktu') ? ' has-danger' : '' }}">
                                     <label class="text-gray-900 font-weight-bold" for="tanggal">Tenggat Waktu</label>
-                                    <input type="date" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tenggat Waktu">
+                                    <input type="date" name="tenggatwaktu" class="form-control form-control-alternative{{ $errors->has('tenggatwaktu') ? ' is-invalid' : '' }}" placeholder="Masukkan Tenggat Waktu" value="" id="input-tenggatwaktu" required/>
+                                    @if ($errors->has('tenggatwaktu'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('tenggatwaktu') }}</strong>
+                                        </span>
+                                    @endif
                                 </div>
 
-                                <a href="halamanProker.php" class=" d-sm-inline-block btn btn-success shadow-sm ">
+                                <div class="d-sm-inline-block btn btn-primary shadow-sm mt-2 mb-4">
+                                    <button type="submit" style="
+                                        background-color: transparent;
+                                        border: none;
+                                        color: inherit;
+                                        cursor: pointer;">{{ __('Simpan Perubahan') }}</button>
+                                </div>
+                                {{-- <a href="halamanProker.php" class=" d-sm-inline-block btn btn-success shadow-sm ">
                                     <i class="fas fa-sm text-white-50"></i>Simpan Perubahan
-                                </a>
+                                </a> --}}
 
                             </form>
                         </div>
@@ -63,3 +95,12 @@
 
 </body>
 </html>
+{{-- <div class="mb-3 form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+    <label class="small mb-1 form-control-label" for="input-name">{{ __('Nama Lengkap') }}</label>
+    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder={{ __('Masukkan Nama Lengkap') }} value="{{ auth()->user()->name }}" required autofocus/>
+    @if ($errors->has('name'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('name') }}</strong>
+        </span>
+    @endif
+</div> --}}
