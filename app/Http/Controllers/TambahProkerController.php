f094a99d5410991d207a5fc1a3991dc75b5aa380
= FebryanRizki24 <= febryanrizki24@gmail.com>
2023-06-09 13:35:37 +0700

    frontend

--- app/Http/Controllers/TambahProkerController.php
+++ app/Http/Controllers/TambahProkerController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Oki;
 use Illuminate\Http\Request;
 
 class TambahProkerController extends Controller
 {
     public function index()
     {
-        return view('tambahProker');
+        $dataOki = Oki::all();
+        return view('tambahProker', compact('dataOki'));
     }
 }
