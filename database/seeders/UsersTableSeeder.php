53349523c437fc16ade0b9f313a2e0aabe51992e
BrianMohamadSafiudin <brianms2004@gmail.com>
2023-06-15 12:56:50 +0700

    backend

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -99,7 +99,7 @@ class UsersTableSeeder extends Seeder
             'image' => 'public/users/m1ZX2MOyPW5p7AIahhb2wXXxX6LYKb1UIM5BG1Pq.jpg'
         ]);
         DB::table('users')->insert([
-            'name' => 'Annisa Nurul Khoiriyah',
+            'name' => 'ANISA NURUL AZIZAH',
             'role' => 'user',
             'nim' => '2141720187',
             'prodi' => 'DIV Teknik Informatika',
